94dd663a62915ad9fdf21ee63bebf036c7d587c0
nurmohammad83 <pesfootball83@gmail.com>
2022-09-20 21:15:28 +0600

    fast

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>All Data is Here</h1>
+      <ExternalComments></ExternalComments>
     </div>
   );
 }
+function ExternalComments(){
+  const [comments, setComments] = useState([])
+  useEffect(()=>{
+    const url=`https://jsonplaceholder.typicode.com/comments`;
+    fetch(url)
+    .then(res=> res.json())
+    .then(data=>setComments(data) )
+  },[])
+  return(
+    <div>
+      <h1>External Comments</h1>
+      <p></p>
+      {
+        comments.map(comment => <User name={comment.name} email={comment.email} body={comment.body.slice(1,200)}></User> )
+      }
+    </div>
+  )
+}
 
+
+
+function User(props){
+  return(
+    <div className="comment">
+      <h2>Name: {props.name}</h2>
+      <h3>Email: {props.email}</h3>
+      <p>{props.body}</p>
+    </div>
+  )
+}
 export default App;
